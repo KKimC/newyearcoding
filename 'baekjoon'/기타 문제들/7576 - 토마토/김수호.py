f5565cf55ec274c47ee99bf8ccbc8e30f3780b16
Unosuh <123133065+Unosuh@users.noreply.github.com>
2023-01-26 21:25:36 +0900

    Update 김수호.py

--- 'baekjoon'/기타 문제들/7576 - 토마토/김수호.py
+++ 'baekjoon'/기타 문제들/7576 - 토마토/김수호.py
@@ -1,12 +1,11 @@
 import copy
 from collections import deque
 
-dx = [-1, 1, 0, 0, 0, 0]
-dy = [0, 0, -1, 1, 0, 0]
-dz = [0, 0, 0, 0, -1, 1]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-m, n, h = map(int, input().split())
-graph = [[list(map(int, input().split())) for _ in range(n)] for _ in range(h)]
+m, n = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
 
 def tomato(q):
     global graph
@@ -15,16 +14,15 @@ def tomato(q):
         
         
     while q:
-        zz, xx, yy, tt = q.popleft()
-        for i in range(6):
+        xx, yy, tt = q.popleft()
+        for i in range(4):
             nx = xx + dx[i]
             ny = yy + dy[i]
-            nz = zz + dz[i]
 
-            if 0<=nx<n and 0<=ny<m and 0<=nz<h:
-                if graph[nz][nx][ny] == 0:
-                    graph[nz][nx][ny] = graph[zz][xx][yy] + 1
-                    q.append((nz, nx, ny, tt+1))
+            if 0<=nx<n and 0<=ny<m:
+                if graph[nx][ny] == 0:
+                    graph[nx][ny] = graph[xx][yy] + 1
+                    q.append((nx, ny, tt+1))
                     if maxx < tt+1:
                         maxx = tt+1
     return maxx
@@ -33,16 +31,14 @@ aa = deque([])
 
 for i in range(n):
     for j in range(m):
-        for k in range(h):
-            if graph[k][i][j] == 1:
-                aa.append((k, i, j, 0))
+        if graph[i][j] == 1:
+            aa.append((i, j, 0))
 AA = tomato(aa)
 TTFF = False
-for k in range(h):
-    for i in range(n):
-        if 0 in graph[k][i]:
-            TTFF = True
-            break
+for i in range(n):
+    if 0 in graph[i]:
+        TTFF = True
+        break
 if TTFF:
     print(-1)
 else:
