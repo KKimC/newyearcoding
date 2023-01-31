a4121e20326c41b1670fef70261cb8cfe7a6253e
Unosuh <123133065+Unosuh@users.noreply.github.com>
2023-01-31 12:54:31 +0900

    Create 김수호.py

---
+++ 'baekjoon'/기타 문제들/2422 - 한윤정이 이탈리아에 가서 아이스크림을 먹는데/김수호.py
@@ -0,0 +1,40 @@
+n, m = map(int, input().split())
+aa = [[] for _ in range(n+1)]
+for _ in range(m):
+    x, y = map(int, input().split())
+    aa[x].append(y)
+    aa[y].append(x)
+icecream = []
+cnt = 0
+
+def dfs(level, number):
+    global cnt
+
+    if level == 0:
+        for i in range(number+1, n-1):
+            if not i in icecream:
+                icecream.append(i)
+                dfs(level+1, i)
+                icecream.pop()
+    elif level == 1:
+        for i in range(number+1, n):
+            if not i in icecream:
+                if not icecream[0] in aa[i]:
+                    icecream.append(i)
+                    dfs(level+1, i)
+                    icecream.pop()
+    elif level == 2:
+        for i in range(number+1, n+1):
+            TF = False
+            if not i in icecream:
+                for xx in icecream:
+                    if xx in aa[i]:
+                        
+                        TF = True
+                        break
+                if TF:
+                    continue
+                cnt += 1
+        return
+dfs(0, 0)
+print(cnt)
